e0d24d486a90906dfe6790c81014533b313b1fb2
VaradharajMurugesan <varadharaj@emergertech.com>
2023-04-13 16:21:09 +0530

    changes in the display message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node testing CICD in Azure Devops!\n'
+  const msg = 'Hello this for testing CI/CD using Azure Devops!\n'
   res.end(msg);
 });
 
