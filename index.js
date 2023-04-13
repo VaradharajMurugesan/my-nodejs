567209e804d13ab305c678cb08de41d8a8794bf3
VaradharajMurugesan <varadharaj@emergertech.com>
2023-04-13 16:43:01 +0530

    changes in message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello this for testing CI/CD using Azure Devops!\n'
+  const msg = 'Hello this for testing CI/CD using Azure Devops....smile!\n'
   res.end(msg);
 });
 
